32d3fef9c1fe77c476c09eddbf7a94fdf5bd16fb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-03-30 20:16:30 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import os
+import numpy as np
+import pandas as pd
+import urllib.request
+from PIL import Image
+
+# Logo and Navigation
+col1, col2, col3 = st.columns((1,4,1))
+with col2:
+    st.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
+st.markdown('# 30 Days of Streamlit')
+
+days_list = [f'Day {x}' for x in list(np.arange(1,31))]
+selected_day = st.selectbox('Start the Challenge 👇', days_list)
+
+with st.expander("About the #30DaysOfStreamlit"):
+    st.markdown('''
+    The **#30DaysOfStreamlit** is a coding challenge designed to help you get started in building Streamlit apps.
+    
+    Particularly, you'll be able to:
+    - Set up a coding environment for building Streamlit apps
+    - Build your first Streamlit app
+    - Learn about all the awesome input/output widgets to use for your Streamlit app
+    ''')
+
+# Sidebar
+st.sidebar.header('About')
+st.sidebar.markdown('[Streamlit](https://streamlit.io) is a Python library that allows the creation of interactive, data-driven web applications in Python.')
+
+st.sidebar.header('Resources')
+st.sidebar.markdown('''
+- [Streamlit Documentation](https://docs.streamlit.io/)
+- [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
+- [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
+- [Blog](https://blog.streamlit.io/how-to-master-streamlit-for-data-science/) (How to master Streamlit for data science)
+''')
+
+st.sidebar.header('Deploy')
+st.sidebar.markdown('With a few clicks of the mouse, Streamlit apps can be easily deployed using [Streamlit Cloud](https://streamlit.io/cloud).')
+
+# Display content
+for i in days_list:
+    if selected_day == i:
+        st.markdown(f'# 🗓️ {i}')
+        j = i.replace(' ', '')
+        with open(f'content/{j}.md', 'r') as f:
+            st.markdown(f.read())
+        if os.path.isfile(f'content/figures/{j}.csv') == True:
+            st.markdown('---')
+            st.markdown('### Figures')
+            df = pd.read_csv(f'content/figures/{j}.csv', engine='python')
+            for i in range(len(df)):
+                st.image(f'content/images/{df.img[i]}')
+                st.info(f'{df.figure[i]}: {df.caption[i]}')

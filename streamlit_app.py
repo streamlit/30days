e080cf53be383af04902a373fec892ccc38254ec
snehankekre <snehan.minerva@gmail.com>
2023-03-09 18:56:36 +0530

    Catch KeyError when query param is invalid

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,67 +1,85 @@
-import streamlit as st
+import glob
 import os
+import urllib.request
+
 import numpy as np
 import pandas as pd
-import urllib.request
+import streamlit as st
 from PIL import Image
-import glob
+
 
 def update_params():
     st.experimental_set_query_params(challenge=st.session_state.day)
 
-md_files = sorted([int(x.strip('Day').strip('.md')) for x in glob.glob1('content',"*.md") ])
+
+md_files = sorted(
+    [int(x.strip("Day").strip(".md")) for x in glob.glob1("content", "*.md")]
+)
 
 # Logo and Navigation
-col1, col2, col3 = st.columns((1,4,1))
+col1, col2, col3 = st.columns((1, 4, 1))
 with col2:
-    st.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
-st.markdown('# 30 Days of Streamlit')
+    st.image(Image.open("streamlit-logo-secondary-colormark-darktext.png"))
+st.markdown("# 30 Days of Streamlit")
 
-days_list = [f'Day {x}' for x in md_files]
+days_list = [f"Day {x}" for x in md_files]
 
 query_params = st.experimental_get_query_params()
 
-if query_params and query_params["challenge"][0] in days_list:
-    st.session_state.day = query_params["challenge"][0]
+try:
+    if query_params and query_params["challenge"][0] in days_list:
+        st.session_state.day = query_params["challenge"][0]
+except KeyError:
+    st.session_state.day = days_list[0]
 
-selected_day = st.selectbox('Start the Challenge 👇', days_list, key="day", on_change=update_params)
+selected_day = st.selectbox(
+    "Start the Challenge 👇", days_list, key="day", on_change=update_params
+)
 
 with st.expander("About the #30DaysOfStreamlit"):
-    st.markdown('''
+    st.markdown(
+        """
     The **#30DaysOfStreamlit** is a coding challenge designed to help you get started in building Streamlit apps.
     
     Particularly, you'll be able to:
     - Set up a coding environment for building Streamlit apps
     - Build your first Streamlit app
     - Learn about all the awesome input/output widgets to use for your Streamlit app
-    ''')
+    """
+    )
 
 # Sidebar
-st.sidebar.header('About')
-st.sidebar.markdown('[Streamlit](https://streamlit.io) is a Python library that allows the creation of interactive, data-driven web applications in Python.')
+st.sidebar.header("About")
+st.sidebar.markdown(
+    "[Streamlit](https://streamlit.io) is a Python library that allows the creation of interactive, data-driven web applications in Python."
+)
 
-st.sidebar.header('Resources')
-st.sidebar.markdown('''
+st.sidebar.header("Resources")
+st.sidebar.markdown(
+    """
 - [Streamlit Documentation](https://docs.streamlit.io/)
 - [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
 - [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
 - [Blog](https://blog.streamlit.io/how-to-master-streamlit-for-data-science/) (How to master Streamlit for data science)
-''')
+"""
+)
 
-st.sidebar.header('Deploy')
-st.sidebar.markdown('You can quickly deploy Streamlit apps using [Streamlit Community Cloud](https://streamlit.io/cloud) in just a few clicks.')
+st.sidebar.header("Deploy")
+st.sidebar.markdown(
+    "You can quickly deploy Streamlit apps using [Streamlit Community Cloud](https://streamlit.io/cloud) in just a few clicks."
+)
 
 # Display content
 for i in days_list:
     if selected_day == i:
-        st.markdown(f'# 🗓️ {i}')
-        j = i.replace(' ', '')
-        with open(f'content/{j}.md', 'r') as f:
+        st.markdown(f"# 🗓️ {i}")
+        j = i.replace(" ", "")
+        with open(f"content/{j}.md", "r") as f:
             st.markdown(f.read())
-        if os.path.isfile(f'content/figures/{j}.csv') == True:
-            st.markdown('---')
-            st.markdown('### Figures')
-            df = pd.read_csv(f'content/figures/{j}.csv', engine='python')
+        if os.path.isfile(f"content/figures/{j}.csv") == True:
+            st.markdown("---")
+            st.markdown("### Figures")
+            df = pd.read_csv(f"content/figures/{j}.csv", engine="python")
             for i in range(len(df)):
-                st.image(f'content/images/{df.img[i]}')
-                st.info(f'{df.figure[i]}: {df.caption[i]}')
+                st.image(f"content/images/{df.img[i]}")
+                st.info(f"{df.figure[i]}: {df.caption[i]}")
